376063a7de1a96fee99ea47108a34b4f263bad38
Stanley Yang <stanley.yang.sy@gmail.com>
2015-02-17 23:26:46 -0500

    finished basic webpage layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-console.log('This would be the main JS file.');
+console.log('Main JS file loaded');
+
+function test(){
+  $("#inputTextArea").val("test in");
+  $("#outputTextArea").val("test out");
+}
