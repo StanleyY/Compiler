db5d7ed7f661428cfa44785bbc69cfcb01d69ad5
Stanley Yang <stanley.yang.sy@gmail.com>
2015-02-18 00:20:12 -0500

    DOM variable references working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,23 @@
-console.log('Main JS file loaded');
+console.log("Main JS file loaded");
+//Globals
+OUTPUT = null;
+INPUT = null;
+
+// setup the globals
+function init(){
+  OUTPUT = $('#outputTextArea');
+  INPUT = $('#inputTextArea');
+}
 
 function test(){
-  $("#inputTextArea").val("test in");
-  $("#outputTextArea").val("test out");
+  lexer(INPUT.val());
+}
+
+function lexer(input){
+  console.log(input);
+  raiseFatalError(input);
+}
+
+function raiseFatalError(message){
+  OUTPUT.append("Fatal Error: " + message + "\n");
 }
