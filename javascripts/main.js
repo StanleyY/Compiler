fce39a267f4bf30d36d4abc5ccf874ef83515b11
Stanley Yang <stanley.yang.sy@gmail.com>
2015-02-19 01:46:46 -0500

    checking for single char variable names done

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,10 @@ console.log("Main JS file loaded");
 //Globals
 OUTPUT = null;
 INPUT = null;
+LINES = null;
 
 //Regex Constants
-TYPE = 'int|string|boolean';
+TYPE = '(int|string|boolean)';
 CHAR = 'a-z';
 SPACE = '\\s';
 DIGIT = '\\d';
@@ -14,42 +15,60 @@ INTOP = '\\+';
 BLOCKS = "\\" + ["{", "}", "(", ")", "\"", "\""].join("\\"); // Curly Brace,
 
 INVALID = "[^" + CHAR + SPACE + DIGIT + INTOP + BLOCKS + "!" + "=" + "$" + "]";
+INVALID_ASSIGNMENT = '.*((int|boolean|string)\\s+(\\d|[a-z]\\w+)).*'
 
 
 // setup the globals
 function init(){
   OUTPUT = $('#outputTextArea');
   INPUT = $('#inputTextArea');
+  LINES = INPUT.val().split("\n");
 }
 
 function test(){
+  init();
   OUTPUT.empty();  // Clear the output text area.
   lexer(INPUT.val());
 }
 
 function lexer(input){
-  checkInvalids();
+  //checkInvalids();
+  checkAssignments();
   //console.log(input.split("\n"));
   //raiseFatalError(input);
 }
 
 function checkInvalids(){
-  var lines = INPUT.val().split("\n");
   var line = 0;
   var invalid_re = new RegExp(INVALID, "g");
   var invalid_check;
 
-  while(line < lines.length){
-    invalid_check = invalid_re.exec(lines[line]);
-    console.log(lines[line]);
+  while(line < LINES.length){
+    invalid_check = invalid_re.exec(LINES[line]);
+    console.log(LINES[line]);
     if(invalid_check != null) raiseFatalError("Invalid character found at line: " + (line + 1) );
     line++;
   }
   writeOutput("No invalid characters found.");
 }
 
+function checkAssignments(){
+  var line = 0;
+  var invalid_re = new RegExp(INVALID_ASSIGNMENT, "g");
+  console.log(invalid_re);
+  var invalid_check;
+
+  while(line < LINES.length){
+    invalid_check = invalid_re.exec(LINES[line]);
+    console.log(LINES[line]);
+    if(invalid_check != null) raiseFatalError("Invalid assignment found at line: " + (line + 1) );
+    line++;
+  }
+  writeOutput("No invalid assignments found.");
+}
+
 function writeOutput(message){
-  OUTPUT.append(message);
+  OUTPUT.append(message + "\n");
 }
 
 function raiseFatalError(message){
