ff310b47329b113dcf0e82b60c078c46a31e9f27
Stanley Yang <stanley.yang.sy@gmail.com>
2015-04-12 02:56:45 -0400

    included code for injecting infovis graph

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,10 +8,20 @@ TOKENS = [];
 PARSE_POSITION = 0;
 CST = null;
 
+// This format function was taken from StackOverflow.
+String.prototype.format = function() {
+    var formatted = this;
+    for( var arg in arguments ) {
+        formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+    }
+    return formatted;
+};
+
 function init(){
   $('#inputTextArea').linedtextarea();
   resetPage();
   defaultTestCase();
+  generateCSTGraph(CST);
 }
 
 function resetPage(){
@@ -21,6 +31,7 @@ function resetPage(){
   TOKENS = [];
   PARSE_POSITION = 0;
   OUTPUT.empty();  // Clear the output text area.
+  $('#infovis').empty();
 }
 
 function run(){
@@ -28,6 +39,7 @@ function run(){
   lexer();
   parser();
   OUTPUT.scrollTop(0);
+  if(CST != null) generateCSTGraph(CST);
 }
 
 function writeOutput(message){
@@ -44,4 +56,50 @@ function raiseFatalError(message){
   writeOutput(message);
   OUTPUT.scrollTop(OUTPUT[0].scrollHeight); // Scroll to the bottom.
   throw new Error(message);
+}
+
+function generateCSTGraph(CST){
+  var json = {"id": "node",  "name": "1",  "data": {},"children": [{"id": "node1",  "name": "2",  "data": {},"children": []}]};
+
+  var st = new $jit.ST({
+      injectInto: 'infovis',
+      orientation: "top",
+      duration: 500,
+      transition: $jit.Trans.Quart.easeInOut,
+      constrained: false,
+      levelsToShow: 10,
+      levelDistance: 10,
+      Navigation: {
+        enable:true,
+        panning:true
+      },
+      Node: {
+          height: 20,
+          width: 60,
+          type: 'rectangle',
+          color: '#aaa',
+          overridable: true
+      },
+
+      Edge: {
+          type: 'bezier',
+          overridable: true
+      },
+
+      onCreateLabel: function(label, node){
+          label.id = node.id;
+          label.innerHTML = node.name;
+          var style = label.style;
+          style.width = 60 + 'px';
+          style.height = 17 + 'px';
+          style.cursor = 'pointer';
+          style.color = '#333';
+          style.fontSize = '0.8em';
+          style.textAlign= 'center';
+          style.paddingTop = '3px';
+      },
+  });
+  st.loadJSON(json);
+  st.compute();
+  st.onClick(st.root);
 }
