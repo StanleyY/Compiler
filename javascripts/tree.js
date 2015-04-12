f53189bbad2583739332121f399097672339ca5e
Stanley Yang <stanley.yang.sy@gmail.com>
2015-04-11 22:54:38 -0400

    added basic print out function to see the levels of the tree

--- javascripts/tree.js
+++ javascripts/tree.js
@@ -3,14 +3,31 @@ console.log("Tree JS loaded.");
 function TreeNode(value) {
   this.val = value;
   this.children = [];
-  this.getValue = getValue;
   this.addChild = addChild;
 }
 
-function getValue(){
-  return this.val;
+function addChild(new_child){
+  this.children.push(new_child);
 }
 
-function addChild(new_child){
-  this.children.push(new_node);
+function printTree(root){
+  current_level = [];
+  next_level = [];
+  current_node = null;
+  current_level.push(root);
+  output_string = "";
+
+  while(current_level.length != 0){
+    current_node = current_level.pop();
+    output_string += current_node.val + ", ";
+    if(current_node.children.length > 0){
+      next_level.push.apply(next_level, current_node.children);
+    }
+    if(current_level == 0) {
+      console.log(output_string);
+      output_string = "";
+      current_level = next_level;
+      next_level = [];
+    }
+  }
 }
