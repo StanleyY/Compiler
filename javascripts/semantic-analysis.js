c04a3d83f6b910b498776599729ef3b1bf6662a9
Stanley Yang <stanley.yang.sy@gmail.com>
2015-04-14 01:01:37 -0400

    Started AssignAST, IntExpr working

--- javascripts/semantic-analysis.js
+++ javascripts/semantic-analysis.js
@@ -36,6 +36,7 @@ function generateStmtAST(ast_node, stmt_node){
   var type = stmt_node.getChild(0).val;
   if(type == "Block") generateBlockAST(ast_node, stmt_node.getChild(0));
   else if(type == "VarDecl") generateVarDeclAST(ast_node, stmt_node.getChild(0));
+  else if(type == "AssignStmt") generateAssignAST(ast_node, stmt_node.getChild(0));
   else {
     return;
   }
@@ -47,10 +48,41 @@ function generateVarDeclAST(ast_node, var_decl_node){
   ast_node.addChild(getIDAST(var_decl_node.getChild(1)));
 }
 
+function generateAssignAST(ast_node, assign_node){
+  ast_node = generateNewChild(ast_node, "Assign");
+  ast_node.addChild(getIDAST(assign_node.getChild(0)));
+  generateExprAST(ast_node, assign_node.getChild(2));
+}
+
+function generateExprAST(ast_node, expr_node){
+  var type = expr_node.getChild(0).val;
+  if(type == "IntExpr") generateIntExprAST(ast_node, expr_node.getChild(0));
+  //else if(type == "StringExpr") ;
+  //else if(type == "BooleanExpr") ;
+  else ast_node.addChild(getIDAST(expr_node.getChild(0)));
+}
+
+function generateIntExprAST(ast_node, int_expr_node){
+  if(int_expr_node.children.length == 1) ast_node.addChild(getDigitAST(int_expr_node.getChild(0)));
+  else{
+    ast_node = generateNewChild(ast_node, getIntOpAST(int_expr_node.getChild(1)));
+    ast_node.addChild(getDigitAST(int_expr_node.getChild(0)));
+    generateExprAST(ast_node, int_expr_node.getChild(2));
+  }
+}
+
 function getTypeAST(node){
   return node.getChild(0);
 }
 
 function getIDAST(node){
   return node.getChild(0).getChild(0);
+}
+
+function getDigitAST(node){
+  return node.getChild(0);
+}
+
+function getIntOpAST(node){
+  return node.getChild(0).val;
 }
