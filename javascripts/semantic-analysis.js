b06716a1312a42ccb3ce4592ef215dcff4adcf03
Stanley Yang <stanley.yang.sy@gmail.com>
2015-04-14 00:42:54 -0400

    VarDecl AST generation working, changed AST related functions' naming style

--- javascripts/semantic-analysis.js
+++ javascripts/semantic-analysis.js
@@ -9,32 +9,48 @@ function generateAST(cst){
   // The first node will be a program node with only a single block child.
   if(CST.children[0].val != "Block") raiseFatalError("Horrible things happened");
   var root = new TreeNode("root");
-  generateBlockChildren(root, CST.getChild(0));
+  generateBlockAST(root, CST.getChild(0));
   root = root.getChild(0); // Root only exists as a place holder.
   return root;
 }
 
-function generateBlockChildren(ast_node, block_node){
+function generateBlockAST(ast_node, block_node){
   ast_node = generateNewChild(ast_node, "Block");
   // Ignoring the first and last children because they are braces.
   for(var i = 1; i < block_node.children.length - 1; i++){
-    generateStmtListChildren(ast_node, block_node.getChild(i));
+    generateStmtListAST(ast_node, block_node.getChild(i));
   }
 }
 
-function generateStmtListChildren(ast_node, stmt_list_node){
+function generateStmtListAST(ast_node, stmt_list_node){
   if(stmt_list_node.children.length === 2) {
-    generateStmtChildren(ast_node, stmt_list_node.getChild(0));
-    generateStmtListChildren(ast_node, stmt_list_node.getChild(1));
+    generateStmtAST(ast_node, stmt_list_node.getChild(0));
+    generateStmtListAST(ast_node, stmt_list_node.getChild(1));
   }
   else{
     // Epsilons are not added to the AST.
   }
 }
 
-function generateStmtChildren(ast_node, stmt_node){
-  if(stmt_node.getChild(0).val == "Block") return generateBlockChildren(ast_node, stmt_node.getChild(0));
+function generateStmtAST(ast_node, stmt_node){
+  var type = stmt_node.getChild(0).val;
+  if(type == "Block") generateBlockAST(ast_node, stmt_node.getChild(0));
+  else if(type == "VarDecl") generateVarDeclAST(ast_node, stmt_node.getChild(0));
   else {
     return;
   }
+}
+
+function generateVarDeclAST(ast_node, var_decl_node){
+  ast_node = generateNewChild(ast_node, "VarDecl");
+  ast_node.addChild(getTypeAST(var_decl_node.getChild(0)));
+  ast_node.addChild(getIDAST(var_decl_node.getChild(1)));
+}
+
+function getTypeAST(node){
+  return node.getChild(0);
+}
+
+function getIDAST(node){
+  return node.getChild(0).getChild(0);
 }
