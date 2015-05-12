4cf7f7dfa9370cd38e0f0c0742b4f04149f31320
Stanley Yang <stanley.yang.sy@gmail.com>
2015-05-12 11:54:43 -0400

    created functions for Op codes

---
+++ javascripts/op-codes.js
@@ -0,0 +1,45 @@
+console.log("Op Codes loaded");
+
+function loadAccConst(value){
+  writeToOutputString("A9" + value);
+}
+
+function loadAccMem(address){
+  writeToOutputString("AD" + address);
+}
+
+function storeAccMem(address){
+  writeToOutputString("8D" + address);
+}
+
+function loadXConst(value){
+  writeToOutputString("A2" + value);
+}
+
+function loadXMem(address){
+  writeToOutputString("AE" + address);
+}
+
+function loadYConst(value){
+  writeToOutputString("A0" + value);
+}
+
+function loadYMem(address){
+  writeToOutputString("AC" + address);
+}
+
+function compareMemToX(address){
+  writeToOutputString("EC" + address);
+}
+
+function jumpBytes(value){
+  writeToOutputString("D0" + value);
+}
+
+function increaseMem(address){
+  writeToOutputString("EE" + address);
+}
+
+function sysCall(){
+  writeToOutputString("FF");
+}
